b51fba27bc0c1a58b1d3b0fd6d36d05c2a4c0041
Kapil Raj <103250862+raj-kapil@users.noreply.github.com>
2026-05-27 11:21:50 +0530

    Add: islands-and-treasure - submission-0

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-0.py
@@ -0,0 +1,58 @@
+class Solution:
+    ## THIS IS DFS SOLUTION
+    # def dfs(self, grid, r, c, distance):
+    #     if r < 0 or c < 0 or r >= len(grid) or c >= len(grid[r]) or grid[r][c] == -1:
+    #         return 
+        
+    #     if grid[r][c] < distance:
+    #         return 
+    #     # update the current distance in the cell
+
+    #     grid[r][c] = distance
+    #     sides = [
+    #         (r-1, c),
+    #         (r+1, c),
+    #         (r, c-1),
+    #         (r, c+1)
+    #     ]
+    #     for row, col in sides:
+    #         self.dfs(grid, row, col, distance + 1)
+
+    # def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+    #     row_len = len(grid)
+    #     col_len = len(grid[0])
+
+    #     for i in range(row_len):
+    #         for j in range(col_len):
+    #             if grid[i][j] == 0:
+    #                 self.dfs(grid, i, j , 0)
+
+    # THIS IS BFS SOLUTION
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        from collections import deque
+        row_len, col_len = len(grid), len(grid[0])
+        q = deque()
+
+        for i in range(row_len):
+            for j in range(col_len):
+                if grid[i][j] == 0:
+                    q.append((i, j))
+
+        
+
+        while q:
+            r, c = q.popleft()
+            sides = [
+                (r+1, c),
+                (r-1, c),
+                (r, c+1),
+                (r, c-1)
+            ]
+            for row, col in sides:
+                if row < 0 or col < 0 or row >= len(grid) or col >= len(grid[row]) or grid[row][col] != 2147483647:
+                    continue
+                grid[row][col] = grid[r][c] + 1
+                q.append((row, col))
+
+
+
