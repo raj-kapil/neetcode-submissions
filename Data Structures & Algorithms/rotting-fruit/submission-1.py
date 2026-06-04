44703023632daa5da53edfb58dcd4347e60b6816
Kapil Raj <103250862+raj-kapil@users.noreply.github.com>
2026-06-05 02:37:26 +0530

    Add: rotting-fruit - submission-1

---
+++ Data Structures & Algorithms/rotting-fruit/submission-1.py
@@ -0,0 +1,94 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        from collections import deque
+
+        q = deque()
+        fresh = 0
+
+        minutes = 0
+
+        row_len, col_len = len(grid), len(grid[0])
+
+        for i in range(row_len):
+            for j in range(col_len):
+                if grid[i][j] == 1:
+                    fresh += 1
+                elif grid[i][j] == 2:
+                    q.append((i,j))
+
+        while q and fresh > 0:
+   
+            # One BFS level = one minute. len(q) freezes the current level,
+            #  and minutes += 1 happens after the whole level spreads infection. 🍊
+            for _ in range(len(q)):
+                r, c = q.popleft()
+
+                sides = [
+                    (r-1, c),
+                    (r+1, c),
+                    (r, c-1),
+                    (r, c+1)
+                ]
+
+                for row, col in sides:
+                    if row < 0 or col < 0 or row >= row_len or col >= col_len or grid[row][col] != 1:
+                        continue
+                    grid[row][col] = 2
+                    fresh -= 1
+                    q.append((row, col))
+            minutes += 1
+        
+        return minutes if fresh ==0 else -1
+
+
+
+
+
+
+
+
+
+
+
+        # from collections import deque 
+
+        # q = deque()
+        # fresh = 0
+
+        # row_len, col_len = len(grid), len(grid[0])
+        
+        # # fresh fruits count
+        # # rotten are in quese
+
+        # for x in range(row_len):
+        #     for y in range(col_len):
+        #         if grid[x][y] == 1:
+        #             fresh += 1
+        #         if grid[x][y] == 2:
+        #             q.append((x,y))
+
+        # minutes = 0
+
+        # # while is for processing until no more nodes exits
+        # while q and fresh > 0:
+        #     # FOR loop: we are processing exactly one BFS level here
+        #     for _ in range(len(q)):
+        #         r, c = q.popleft()
+        #         sides = [
+        #             (r -1, c),
+        #             (r +1, c),
+        #             (r, c -1),
+        #             (r, c +1)
+        #         ]
+
+        #         for row, col in sides:
+        #             if row < 0 or col < 0 or row >= row_len or col >= col_len or grid[row][col] != 1:
+        #                 continue
+                    
+        #             grid[row][col] = 2
+        #             fresh -= 1
+        #             q.append((row, col))
+
+        #     minutes += 1
+
+        # return minutes if fresh == 0 else - 1
